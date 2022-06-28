d08e9dc3d3ee3a5a1221e13638845f6f039ea3f5
wuzy <2953108761@qq.com>
2022-06-28 10:31:22 -0400

    Finish Tapping Experiment

--- platform-tools/adb_check.py
+++ platform-tools/adb_check.py
@@ -2,7 +2,17 @@ import cv2
 import subprocess
 import time
 import os
-
+rows=[600,1000,1300,1500]
+cols=[160,320,480,700,900]
+#first col 160
+#second col 320
+#third col 480
+#fourth col 700
+#fifth col 900
+#first row 600
+#second row 1000
+#third row 1300
+#fourth 1500
 def adb(command):
     proc=subprocess.Popen(command.split(' '), stdout=subprocess.PIPE,shell=True)
     (out,_)=proc.communicate()
@@ -14,6 +24,7 @@ def check():
 def swipe(start_x, start_y, end_x, end_y, duration_ms=100):
     adb("adb shell input swipe {} {} {} {} {}".format(start_x, start_y, end_x, end_y, duration_ms))
     time.sleep(1)
+
 def unlock():
     info=adb('adb shell dumpsys window policy')
     getInfo=sorted(info.split())
@@ -37,6 +48,18 @@ def home():
 
 def menu():
     swipe(540,1500,540,500,100)
+
+def image_position(small_image, big_image):
+    img_rgb = cv2.imread(big_image)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(small_image, 0)
+    height, width = template.shape[::]
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_SQDIFF)
+    _, _, top_left, _ = cv2.minMaxLoc(res)
+    bottom_right = (top_left[0] + width, top_left[1] + height)
+    return (top_left[0]+bottom_right[0])//2, (top_left[1]+bottom_right[1])//2
+
+
 ##def install(route):
 ##    adb("adb shell input keyevent 82")
 # unlock()
@@ -45,6 +68,51 @@ def start():
     home()
     menu()
 
-os.system('adb push /Users/ahq09/Desktop/platform-tools/screenshots/. /sdcard/Pictures/')
+def tapDebug():
+    start()
+    tap(cols[1],rows[1])
+def tapMessage():
+    start()
+    tap(cols[0],rows[2])
+def tapPhone():
+    start()
+    tap(cols[2],rows[2])
+def tapPing():
+    start()
+    tap(cols[3],rows[2])
+def tapSettings():
+    start()
+    tap(cols[0],rows[3])
+# os.system('adb push /Users/ahq09/Desktop/platform-tools/screenshots/. /sdcard/Pictures/')
+# screen="screen"
+# final="browserIcon"
+# start()
+# take_screenshot(screen)
+# os.system("adb pull /sdcard/Pictures/screen.png /Users/ahq09/Desktop/platform-tools/screenshots/")
+#
+#
+# x, y  = image_position(f"./screenshots/{final}.png", f"./screenshots/{screen}.png")
+# start()
+# print("x=",x)
+# print("y=",y)
+
+tapDebug()
+tapMessage()
+tapPhone()
+
+tapPing()
+tapSettings()
+#first col 160
+#second col 320
+#third col 480
+#fourth col 700
+#fifth col 900
+#first row 600
+#second row 1000
+#third row 1300
+#fourth 1500
+
+
+
 # home()
 # take_screenshot("screen")
