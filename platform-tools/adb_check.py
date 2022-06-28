375f5930a3d4d82fadd851c2df08fc0d7dea846e
wuzy <2953108761@qq.com>
2022-06-28 09:32:53 -0400

    start and screenshots

--- platform-tools/adb_check.py
+++ platform-tools/adb_check.py
@@ -1,7 +1,7 @@
 import cv2
 import subprocess
 import time
-
+import os
 
 def adb(command):
     proc=subprocess.Popen(command.split(' '), stdout=subprocess.PIPE,shell=True)
@@ -11,19 +11,26 @@ def adb(command):
 def check():
     adb("adb devices")
 
-def swipe(start_x, start_y, end_x, end_y, duration_ms):
+def swipe(start_x, start_y, end_x, end_y, duration_ms=100):
     adb("adb shell input swipe {} {} {} {} {}".format(start_x, start_y, end_x, end_y, duration_ms))
-
-def unlock():
-    adb("adb shell input keyevent 82")
     time.sleep(1)
-    swipe(540,2000,540,200,100)
+def unlock():
+    info=adb('adb shell dumpsys window policy')
+    getInfo=sorted(info.split())
+    if 'screenState=SCREEN_STATE_OFF' in getInfo:
+        adb('adb shell input keyevent 82')
+        time.sleep(1)
+        swipe(540,2000,540,200,100)
+    else:
+        pass
 
 def take_screenshot(final):
-    adb("adb exec-out screencap -p /sdcard/Pictures/{final}.png")
+    adb(f"adb exec-out screencap -p /sdcard/Pictures/{final}.png")
+    time.sleep(1)
 
 def tap(tap_x, tap_y):
     adb("adb shell input tap {} {}".format(tap_x, tap_y))
+    time.sleep(1)
 
 def home():
     tap(540,2300)
@@ -33,8 +40,11 @@ def menu():
 ##def install(route):
 ##    adb("adb shell input keyevent 82")
 # unlock()
-# unlock()
-# screen='screen'
-# take_screenshot(screen)
-# unlock()
-take_screenshot("debug")
+def start():
+    unlock()
+    home()
+    menu()
+
+os.system('adb push /Users/ahq09/Desktop/platform-tools/screenshots/. /sdcard/Pictures/')
+# home()
+# take_screenshot("screen")
