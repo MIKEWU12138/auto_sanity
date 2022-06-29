f6329f3bdf9b3e7ecead3d70c065d61cd6af1605
wuzy <2953108761@qq.com>
2022-06-29 13:05:12 -0400

    log and call

--- platform-tools/adb_check.py
+++ platform-tools/adb_check.py
@@ -17,6 +17,24 @@ def adb(command):
     proc=subprocess.Popen(command.split(' '), stdout=subprocess.PIPE,shell=True)
     (out,_)=proc.communicate()
     return out.decode('utf-8')
+def installPingTool():
+    # adb('adb install "C:\Users\ahq09\Desktop\PingTools Network Utilities_v4.52 Free_apkpure.com.apk"')
+    # time.sleep(8)
+    pass
+
+def getControl():
+    pass
+    # adb("adb shell < /Users/ahq09/Desktop/platform-tools/getControl.bat")
+    # adb("adb shell r")
+    # os.system("adb shell")
+    # os.system("su root")
+    # procId = subprocess.Popen('adb shell', stdin = subprocess.PIPE,shell=True)
+    # procId.communicate('su root\n')
+    # p1 = subprocess.Popen('adb shell&&su root&&cd data&&ls |grep com', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def commandInsideShell(command):
+    proc=subprocess.Popen(command, stdout=subprocess.PIPE)
+    proc.stdout.read()
 
 def check():
     adb("adb devices")
@@ -44,7 +62,9 @@ def tap(tap_x, tap_y):
     time.sleep(1)
 
 def home():
-    tap(540,2300)
+    # tap(540,2300)
+    adb("adb shell input keyevent 3")
+    time.sleep(1)
 
 def menu():
     swipe(540,1500,540,500,100)
@@ -70,19 +90,68 @@ def start():
 
 def tapDebug():
     start()
-    tap(cols[1],rows[1])
+    tap(cols[0],rows[1])
 def tapMessage():
     start()
-    tap(cols[0],rows[2])
+    tap(cols[4],rows[1])
 def tapPhone():
     start()
-    tap(cols[2],rows[2])
+    tap(cols[1],rows[2])
 def tapPing():
     start()
-    tap(cols[3],rows[2])
+    tap(cols[2],rows[2])
 def tapSettings():
     start()
-    tap(cols[0],rows[3])
+    tap(cols[4],rows[2])
+
+def logPlay():
+    tapDebug()
+    tap(540,2200)
+    time.sleep(10)
+
+def logStop():
+    tapDebug()
+    tap(540,2200)
+    time.sleep(15)
+
+def renameAndCompress(type):
+    logPlay()
+    logStop()
+    command1="adb shell cd sdcard"
+    command2="mv debuglogger %s"%type
+    command3="tar -czvf %s.zip ./%s"%(type,type)
+    command=command1+"&&"+command2+"&&"+command3
+    # command=command1+"&&"+command2+"&&"+command3+
+    proc=subprocess.Popen(command, stdout=subprocess.PIPE)
+    proc.stdout.read()
+
+# adb("adb shell service call phone 1 s16 %s"%"7345103190")
+# tap(540,2200)
+# time.sleep(3)
+# tap(480,1250)
+# time.sleep(10)
+# adb("adb shell input keyevent 6")
+
+# tap(1000 ,1500)#6
+# tap(600 ,1500)#5
+# tap(200 ,1500)#4
+adb("adb shell settings put global multi_sim_voice_call 3")
+adb("adb shell am start -a android.intent.action.CALL -d tel:7345103190")
+time.sleep(10)
+adb("adb shell settings put global multi_sim_voice_call 4")
+adb("adb shell am start -a android.intent.action.CALL -d tel:7345103190")
+
+# t2="2"
+# t3="3"
+# str="%s+%s=%s"%(t1,t2,t3)
+# print(str)
+# logPlay()
+# os.system('adb push /Users/ahq09/Desktop/platform-tools/ /sdcard/Pictures/')
+# proc=subprocess.Popen("adb shell cd sdcard&&mv debuglogger fuck", stdout=subprocess.PIPE)
+    # print (proc.stdout.read())
+# command1="adb shell cd sdcard"
+# command2="mv fuck fuck1"
+# commandInsideShell(command1+"&&"+command2)
 # os.system('adb push /Users/ahq09/Desktop/platform-tools/screenshots/. /sdcard/Pictures/')
 # screen="screen"
 # final="browserIcon"
@@ -96,12 +165,12 @@ def tapSettings():
 # print("x=",x)
 # print("y=",y)
 
-tapDebug()
-tapMessage()
-tapPhone()
-
-tapPing()
-tapSettings()
+# tapDebug()
+# tapMessage()
+# tapPhone()
+#
+# tapPing()
+# tapSettings()
 #first col 160
 #second col 320
 #third col 480
